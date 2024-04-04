9b167870dbc0864911ed30b14618e2b411711dc8
blitterated <blitterated@protonmail.com>
2024-04-04 01:33:13 -0500

    Factored everything into KarabinerComplexMod class
    Local vars are now methods on KarabinerComplexMod class.
    Hash#store_local_var is now KarabinerComplexMod#add_hash_node.
    Hash#store_local_var calls are now Hash#then blocks calling KarabinerComplexMod#add_hash_node.

--- complex_mods.rb
+++ complex_mods.rb
@@ -2,158 +2,168 @@
 
 require 'json'
 
-class Hash
-  def store_local_var(key, vsymbol, caller_binding)
-    return nil unless caller_binding.local_variable_defined?(vsymbol)
-    
-    value = caller_binding.local_variable_get(vsymbol)
-    self.store(key,value)
-    self
-  end
-end
+class KarabinerComplexMod
 
-# TODO: 835 is actually "product_id"
-apple_keyboard_conditions = [
-  {
-    :type => "device_if",
-    :identifiers => [
-      {
-        :vendor_id => 1452
-      },
-      {
-        :vendor_id => 835
-      }
-    ]
-  }
-]
+  private
 
+    def add_hash_node(key, method, hash)
+       return hash unless self.private_methods.include?(method)
+       hash[key] = send method
+       hash
+    end
 
-complex_mods = {
-  :title => "Only modify Macbook Pro built in keyboard",
-  :rules => [
-    {
-      :description => "MBP Only: CapsLock is Esc on tap, Ctrl on mod",
-      :manipulators => [
+    # TODO: 835 is actually "product_id"
+    def apple_keyboard_conditions
+      [
         {
-          :type => "basic",
-          :from => {
-            :key_code => "caps_lock",
-            :modifiers => {
-              :optional => [
-                "any"
-              ]
-            }
-          },
-          :to => [
+          :type => "device_if",
+          :identifiers => [
             {
-              :key_code => "left_control"
-            }
-          ],
-          :to_if_alone => [
+              :vendor_id => 1452
+            },
             {
-              :key_code => "escape"
+              :vendor_id => 835
             }
           ]
-        }.store_local_var(:conditions, :apple_keyboard_conditions, binding)
+        }
       ]
-    },
-    {
-      :description => "MBP Only: Shift keys are parens on tap",
-      :manipulators => [
-        {
-          :type => "basic",
-          :from => {
-            :key_code => "left_shift"
-          },
-          :to => [
-            {
-              :key_code => "left_shift"
-            }
-          ],
-          :to_if_alone => [
-            {
-              :key_code => "9",
-              :modifiers => [
-                "left_shift"
-              ]
-            }
-          ],
-        }.store_local_var(:conditions, :apple_keyboard_conditions, binding),
-        {
-          :type => "basic",
-          :from => {
-            :key_code => "right_shift"
-          },
-          :to => [
-            {
-              :key_code => "right_shift"
-            }
-          ],
-          :to_if_alone => [
-            {
-              :key_code => "0",
-              :modifiers => [
-                "right_shift"
-              ]
-            }
-          ],
-        }.store_local_var(:conditions, :apple_keyboard_conditions, binding)
-      ]
-    },
-    {
-      :description => "MBP Only: LS + RS = CapsLock",
-      :manipulators => [
-        {
-          :type => "basic",
-          :from => {
-            :key_code => "left_shift",
-            :modifiers => {
-              :mandatory => [
-                "right_shift"
-              ],
-              :optional => [
-                "caps_lock"
-              ]
-            }
-          },
-          :to => [
-            {
-              :key_code => "caps_lock"
-            }
-          ],
-          :to_if_alone => [
-            {
-              :key_code => "left_shift"
-            }
-          ],
-        }.store_local_var(:conditions, :apple_keyboard_conditions, binding),
-        {
-          :type => "basic",
-          :from => {
-            :key_code => "right_shift",
-            :modifiers => {
-              :mandatory => [
-                "left_shift"
-              ],
-              :optional => [
-                "caps_lock"
-              ]
-            }
-          },
-          :to => [
-            {
-              :key_code => "caps_lock"
-            }
-          ],
-          :to_if_alone => [
-            {
-              :key_code => "right_shift"
-            }
-          ],
-        }.store_local_var(:conditions, :apple_keyboard_conditions, binding)
-      ]
-    }
-  ]
-}
+    end
 
-puts JSON.pretty_generate complex_mods
+    def capslock_to_esc_and_control_rule
+      {
+        :description => "MBP Only: CapsLock is Esc on tap, Ctrl on mod",
+        :manipulators => [
+          {
+            :type => "basic",
+            :from => {
+              :key_code => "caps_lock",
+              :modifiers => {
+                :optional => [
+                  "any"
+                ]
+              }
+            },
+            :to => [
+              {
+                :key_code => "left_control"
+              }
+            ],
+            :to_if_alone => [
+              {
+                :key_code => "escape"
+              }
+            ]
+          }.then { |h| add_hash_node(:conditions, :apple_keyboard_conditions, h) }
+        ]
+      }
+    end
+
+  public
+
+    def generate
+      {
+        :title => "Only modify Macbook Pro built in keyboard",
+        :rules => [
+          capslock_to_esc_and_control_rule,
+          {
+            :description => "MBP Only: Shift keys are parens on tap",
+            :manipulators => [
+              {
+                :type => "basic",
+                :from => {
+                  :key_code => "left_shift"
+                },
+                :to => [
+                  {
+                    :key_code => "left_shift"
+                  }
+                ],
+                :to_if_alone => [
+                  {
+                    :key_code => "9",
+                    :modifiers => [
+                      "left_shift"
+                    ]
+                  }
+                ],
+              }.then { |h| add_hash_node(:conditions, :apple_keyboard_conditions, h) },
+              {
+                :type => "basic",
+                :from => {
+                  :key_code => "right_shift"
+                },
+                :to => [
+                  {
+                    :key_code => "right_shift"
+                  }
+                ],
+                :to_if_alone => [
+                  {
+                    :key_code => "0",
+                    :modifiers => [
+                      "right_shift"
+                    ]
+                  }
+                ],
+              }.then { |h| add_hash_node(:conditions, :apple_keyboard_conditions, h) }
+            ]
+          },
+          {
+            :description => "MBP Only: LS + RS = CapsLock",
+            :manipulators => [
+              {
+                :type => "basic",
+                :from => {
+                  :key_code => "left_shift",
+                  :modifiers => {
+                    :mandatory => [
+                      "right_shift"
+                    ],
+                    :optional => [
+                      "caps_lock"
+                    ]
+                  }
+                },
+                :to => [
+                  {
+                    :key_code => "caps_lock"
+                  }
+                ],
+                :to_if_alone => [
+                  {
+                    :key_code => "left_shift"
+                  }
+                ],
+              }.then { |h| add_hash_node(:conditions, :apple_keyboard_conditions, h) },
+              {
+                :type => "basic",
+                :from => {
+                  :key_code => "right_shift",
+                  :modifiers => {
+                    :mandatory => [
+                      "left_shift"
+                    ],
+                    :optional => [
+                      "caps_lock"
+                    ]
+                  }
+                },
+                :to => [
+                  {
+                    :key_code => "caps_lock"
+                  }
+                ],
+                :to_if_alone => [
+                  {
+                    :key_code => "right_shift"
+                  }
+                ],
+              }.then { |h| add_hash_node(:conditions, :apple_keyboard_conditions, h) }
+            ]
+          }
+        ]
+      }
+    end
+end
+
+puts JSON.pretty_generate(KarabinerComplexMod.new.generate)
