3649ce9ba933f557e4a9b83dfa41adc36fc2e8b1
blitterated <blitterated@protonmail.com>
2024-04-04 03:56:11 -0500

    Refactored build_shift_to_capslock_manipulator() and more
    build_shift_to_capslock_manipulator() now uses build_basic_to_if_alone_manipulator().
    Replaced build_shift_to_capslock_manipulator() with build_shift_key_manipulator_targets().
    build_basic_to_if_alone_manipulator() wraps :to and :to_if_alone in arrays.
    Shortened names of target local vars.

--- complex_mods.rb
+++ complex_mods.rb
@@ -43,8 +43,8 @@ class KarabinerComplexMod
       hash = {
         :type => "basic",
         :from => from_target,
-        :to => to_target,
-        :to_if_alone => to_if_alone_target
+        :to => [to_target],
+        :to_if_alone => [to_if_alone_target]
       }
       
       conditions = build_apple_keyboard_conditions
@@ -55,8 +55,8 @@ class KarabinerComplexMod
     def build_capslock_to_esc_and_control_rule
 
       from_target =        build_manipulator_target(:caps_lock, {:optional => ["any"]})
-      to_target =          [build_manipulator_target(:left_control)]
-      to_if_alone_target = [build_manipulator_target(:escape)]
+      to_target =          build_manipulator_target(:left_control)
+      to_if_alone_target = build_manipulator_target(:escape)
       
       {
         :description => "MBP Only: CapsLock is Esc on tap, Ctrl on mod",
@@ -68,85 +68,48 @@ class KarabinerComplexMod
       }
     end
 
-#   def build_shift_keys_parens_on_tap_manipulator(from_key_code, to_key_code)
-#     {
-#       :type => "basic",
-#       :from => {
-#         :key_code => from_key_code
-#       },
-#       :to => [
-#         {
-#           :key_code => from_key_code
-#         }
-#       ],
-#       :to_if_alone => [
-#         {
-#           :key_code => to_key_code,
-#           :modifiers => [
-#             from_key_code
-#           ]
-#         }
-#       ],
-#     }.then do |h|
-#       add_hash_node(:conditions, :build_apple_keyboard_conditions, h)
-#     end
-#   end
-
     def build_shift_keys_parens_on_tap_rule
 
-      left_target =        build_manipulator_target(:left_shift)
-      left_alone_target =  build_manipulator_target("9", [:left_shift])
-      right_target =       build_manipulator_target(:right_shift)
-      right_alone_target = build_manipulator_target("0", [:right_shift])
+      l_target =       build_manipulator_target(:left_shift)
+      l_alone_target = build_manipulator_target("9", [:left_shift])
+      r_target =       build_manipulator_target(:right_shift)
+      r_alone_target = build_manipulator_target("0", [:right_shift])
 
       {
         :description => "MBP Only: Shift keys are parens on tap",
         :manipulators => [
-          build_basic_to_if_alone_manipulator(left_target, [left_target], [left_alone_target]),
-          build_basic_to_if_alone_manipulator(right_target, [right_target], [right_alone_target])
+          build_basic_to_if_alone_manipulator(l_target, l_target, l_alone_target),
+          build_basic_to_if_alone_manipulator(r_target, r_target, r_alone_target)
         ]
       }
     end
 
-    def build_shift_to_capslock_manipulator(shift_key_code)
-      {
-        :type => "basic",
-        :from => {
-          :key_code => shift_key_code,
-          :modifiers => {
-            :mandatory => [
-              case shift_key_code
-              when :left_shift;   :right_shift
-              when :right_shift;  :left_shift
-              else ;              throw "Not a shift key: #{shift_key_code}"
-              end
-            ],
-            :optional => [
-              :caps_lock
-            ]
-          }
-        },
-        :to => [
-          {
-            :key_code => :caps_lock
-          }
-        ],
-        :to_if_alone => [
-          {
-            :key_code => shift_key_code
-          }
-        ],
-      }.then do |h|
-        add_hash_node(:conditions, :build_apple_keyboard_conditions, h)
-      end
+    def build_shift_key_manipulator_targets(shift_key_code, to_key_code)      
+      opposite_shift_key_code = 
+        case shift_key_code
+        when :left_shift;   :right_shift
+        when :right_shift;  :left_shift
+        else ;              throw "Not a shift key: #{shift_key_code}"
+        end
+      
+      from_mods = {:mandatory => [opposite_shift_key_code], :optional => [to_key_code]}
+      from =      build_manipulator_target(shift_key_code, from_mods)
+      to =        build_manipulator_target(to_key_code)
+      alone =     build_manipulator_target(shift_key_code)
+      
+      [from, to, alone]
     end
 
     def build_both_shifts_to_capslock_rule
+          
+      l_from, l_to , l_alone = build_shift_key_manipulator_targets(:left_shift, :caps_lock) 
+      r_from, r_to , r_alone = build_shift_key_manipulator_targets(:right_shift, :caps_lock) 
+
       {
         :description => "MBP Only: LS + RS = CapsLock",
         :manipulators => [
-          build_shift_to_capslock_manipulator(:left_shift),
-          build_shift_to_capslock_manipulator(:right_shift)
+          build_basic_to_if_alone_manipulator(l_from, l_to, l_alone),
+          build_basic_to_if_alone_manipulator(r_from, r_to, r_alone)
         ]
       }
     end
